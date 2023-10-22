5acf663fdd8cd600af82f1278c3d4bcb0e92bc05
Daymond <daymond716@163.com>
2023-10-22 21:36:57 +0800

    master

--- Sort/Solutions.cpp
+++ Sort/Solutions.cpp
@@ -3,79 +3,43 @@
  * @Author: Daymond
  * @Date: 2023-09-29 20:58:14
  */
-#define MAXSIZE 50
 #include<stdio.h>
 
-
-
-typedef struct SqList{
-    /* data */
-    //顺序表零号位置存放哨兵
-    int key[MAXSIZE+1];
-    int length;
-}SqList;
-
-void InitSqList(SqList *L){
-    L->length = 1;
+void Swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
-void SqListGenerate(SqList * L){
-    int x;
-    scanf("%d",&x);
-    while(x != 9999){
-        if (L->length <= MAXSIZE+1){
-            /* code */
-            L->key[L->length] = x;
-            L->length++;
+/**
+ * @brief 直接插入排序
+ * 
+ * @param arr 
+ * @param len 
+ */
+void InsertSort(int arr[],int len){
+    for(int i = 1;i < len;i++){
+        int key  =arr[i];
+        int j = i-1;
+        while(key < arr[j] && j>= 0){
+            arr[j+1] = arr[j];
+            j--;
         }
-        scanf("%d",&x);   
+        arr[j+1] = key;
     }
-}   
-
-
-void Put(SqList L){
-    for(int i = 1;i < L.length;i++){
-        printf("%d ",L.key[i]);
-    }
-    
 }
+
+
 
 
 
 /**
- * @brief 插入排序--每一步把待排序的对象查到合适的位置
+ * @brief 快速排序
  * 
- * 
+ * @param arr 
+ * @param l 
+ * @param r 
  */
-void Insert_Sort(SqList * list){
-    int i,j;
-    //i=0的位置放哨兵
-    for(i = 1;i <= list->length;i++){
-        //只有后面的元素比前面的小才会进行移位
-        if(list->key[i] < list->key[i-1]){
-            //将第i元素放在顺序表第一个位置
-            list->key[0] = list->key[i];
-            for(j = i - 1;i >= 0 && list->key[j] > list->key[0];j--){
-                list->key[j+1] = list->key[j];
-            }
-        }
-        list->key[j+1] = list->key[0];
-    }
-}
-
-
-/**
- * @brief 折半插入
- * 
- * @param lsit 
- */
-void BInsert_Sort(SqList * lsit){
-    
-}
-
-
-
-
 void QuickSort(int arr[], int l,int r) {
     if (l > r) {
         return;
@@ -102,4 +66,75 @@ void QuickSort(int arr[], int l,int r) {
     }
     QuickSort(arr, l, right - 1);
     QuickSort(arr, right + 1, r);
+}
+
+/**
+ * @brief 调整堆
+ * 
+ * @param arr 
+ * @param k 
+ * @param len 
+ */
+void HeadAdjust(int arr[],int k,int len) {
+    int largest = k;
+    int lson = 2*k+1;
+    int rson = 2*k+2;
+    if (lson < len && arr[largest] < arr[lson]) {
+        largest = lson;
+    }
+    if (rson < len &&  arr[largest] < arr[rson]) {
+        largest = rson;
+    }
+    if (largest != k)  {
+        Swap(arr[k], arr[largest]);
+        HeadAdjust(arr, largest, len);
+    }
+}   
+
+
+/**
+ * @brief Create a Heap object
+ * 
+ * @param arr 
+ * @param len 
+ */
+void CreateHeap(int arr[],int len) {
+    for (int i = len / 2; i >= 0;i-- ) {
+        HeadAdjust(arr, i, len);
+    }
+}
+
+
+/**
+ * @brief 简单选择排序
+ * 
+ * @param arr 
+ * @param len 
+ */
+void SelectSort(int arr[], int len) {
+    int min, i, j;
+    for(int i =0;i < len;i++){
+        min = i;
+        for (j = i + 1; j < len; j++) {
+            if (arr[j] > arr[min]) {
+                min = j;
+            }
+        }
+        Swap(arr[i], arr[min]);
+    }
+}
+
+
+/**
+ * @brief 堆排序
+ * 
+ * @param arr 
+ * @param len 
+ */
+void HeapSort(int arr[], int len) {
+    CreateHeap(arr, len);
+    for (int i = len - 1; i > 0; i--) {
+        Swap(arr[i], arr[0]);
+        HeadAdjust(arr, 0, i);
+    }
 }
--- Sort/main.cpp
+++ Sort/main.cpp
@@ -4,13 +4,40 @@
  * @Date: 2023-09-29 21:14:03
  */
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 #include"Solutions.cpp"
+#define RANDOMNUM 100000
+#define MAX 1000
+
+
 
 int main(){
-    SqList L;
-    InitSqList(&L);
-    SqListGenerate(&L);
-    Put(L);
-    Insert_Sort(&L);
-    Put(L);
+    // int arr[RANDOMNUM];
+    // clock_t start, end;
+    // double cpu_time_used;
+    // srand(time(NULL)); // 设置随机数种子
+    // start = clock(); // 记录开始时间
+    // for (int i = 0; i < RANDOMNUM; i++) {
+    //     arr[i] = rand() % MAX; // 生成 0 到 99 之间的随机数
+    // }
+    // HeapSort(arr, RANDOMNUM);
+    // //QuickSort(arr, 0, RANDOMNUM - 1);
+    // // SelectSort(arr, RANDOMNUM);
+    // end = clock(); // 记录结束时间
+    // cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC; // 计算所用时间（秒）
+    // printf("排序1000000个随机数所用时间: %f 秒\n", cpu_time_used);
+
+    int arr[] = {3,6,3,8,1,4,2,4,6,7,4,23,54,67,34,3};
+    int i;
+    int len = sizeof(arr)/sizeof(arr[0]);
+    for(i = 0;i < len;i++){
+        printf("%d ",arr[i]);
+    }
+    InsertSort(arr,len);
+    printf("\n");
+    for(i = 0;i < len;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
 }
